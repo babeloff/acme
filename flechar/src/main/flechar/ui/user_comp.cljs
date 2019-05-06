2a6e9b3a8c7d2c665383c54670941b0174ffd028
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2019-05-06 16:05:53 -0500

    split out ui-user component from root.

--- flechar/src/main/flechar/ui/user_comp.cljs
+++ flechar/src/main/flechar/ui/user_comp.cljs
@@ -23,18 +23,20 @@
 (prim/defsc UserButton [this {:user/keys [id]}]
   {:query [:user/id]
    :ident [:user/id]}
-  (dom/button :.ui.icon.button
-              {:onClick (fn []
-                          (let [id (str (random-uuid))]
-                            (log/info "Adding user")
-                            ;; NOTE: The lack of quoting works because we're using declare-mutation from incubator. see model.cljs
-                            ;; NOTE 2: This is a "mutation join".  The mutation is sent with a query, and on success it
-                            ;; returns the user.  This allows the server to tack on an address without us specifying it,
-                            ;; and also have the local database/ui update with it.
-                            (prim/transact! this [{(user/upsert-user {:user/id id}
-                                                                     :user/name (str "User " id))}]
-                                            (prim/get-query User))))}
-              (dom/i :.plus.icon)
-              "Add User"))
+  (dom/button
+    :.ui.icon.button
+    {:onClick
+     (fn []
+       (let [id (str (random-uuid))]
+         (log/info "Adding user")
+         ;; NOTE: The lack of quoting works because we're using declare-mutation from incubator. see model.cljs
+         ;; NOTE 2: This is a "mutation join".  The mutation is sent with a query, and on success it
+         ;; returns the user.  This allows the server to tack on an address without us specifying it,
+         ;; and also have the local database/ui update with it.
+         (prim/transact! this [{(user/upsert-user {:user/id   id
+                                                   :user/name (str "User " id)})
+                                (prim/get-query User)}])))}
+    (dom/i :.plus.icon)
+    "Add User"))
 
 (def ui-user-button (prim/factory UserButton))
