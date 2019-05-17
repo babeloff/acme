7bc13fbe1672a9a4c96163e2b11463b267f7ffe1
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2019-05-16 19:39:58 -0500

    trying to use react-cytoscapejs but CytoscapeComponent is export default

--- flechar/src/main/flechar/ui/cyto.cljs
+++ flechar/src/main/flechar/ui/cyto.cljs
@@ -1,50 +1,23 @@
 (ns flechar.ui.cyto
+  "https://github.com/plotly/react-cytoscapejs"
   (:require
-    ;; #?(:clj [fulcro.client.dom-server :as dom]
-    ;;   :cljs [fulcro.client.dom :as dom]
     [fulcro.client.dom :as dom]
     [fulcro.client.primitives :as prim]
-    [react-cytoscapejs :as cy]
-    [fulcro.util :as util]))
+    [flechar.ui.helper :as help]
+    ["react-cytoscapejs" :as recy]))
 
 
 (def elements
-  [ { :data { :id "one", :label "Node 1" }, :position { :x 0, :y 0 } },
-    { :data { :id "two", :label "Node 2" }, :position { :x 100, :y 0 } },
-    { :data { :source "one", :target "two", :label "Edge from Node1 to Node2"}}])
+  [{:data {:id "one", :label "Node 1"}, :position {:x 0, :y 0}},
+   {:data {:id "two", :label "Node 2"}, :position {:x 100, :y 0}},
+   {:data {:source "one", :target "two", :label "Edge from Node1 to Node2"}}])
 
-(defn factory-force-children
-  [class]
-  (fn [props & children]
-    (js/React.createElement
-      class
-      props
-      (util/force-children children))))
-
-(defn factory-apply
-  [class]
-  (fn [props & children]
-    (apply js/React.createElement
-           class
-           props
-           children)))
+(def cytoscape-component (help/factory-default recy))
 
 (prim/defsc Cyto
-  "Generates an Cytoscape graph image.
-
-  ```
-  (ui-cyto {:w 50 :h 50 :label \"avatar\"})
-  ```
-  "
   [_ props]
-  (:initial-state)
+  (cytoscape-component (clj->js {:elements elements
+                                 :style    {:width "600px" :height "600px"}})))
 
-  (let [{:keys [db/id]} props]
-    (let [label (or label (str w "x" h))]
-      (dom/svg #js {:width w :height h}
-               (dom/rect #js {:width w :height h :style #js {:fill "rgb(100,200,200)"
-                                                             :strokeWidth 2
-                                                             :stroke      "black"}})
-               (dom/text #js {:textAnchor "middle" :x (/ w 2) :y (/ h 2)} label)))))
 
-(def ui-svg (prim/factory Svg {:keyfn :svg/label}))
+(def ui-cyto (prim/factory Cyto {:keyfn :cyto/id}))
